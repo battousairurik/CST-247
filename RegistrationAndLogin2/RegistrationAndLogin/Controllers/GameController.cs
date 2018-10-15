968845f3d3365e13d84faa4041dda399d5069c72
Michael Weaver <georgemclinly@yahoo.com>
2018-10-14 21:08:04 -0400

    Fixed Reg and Login with Partially Functioning GameBoard

---
+++ RegistrationAndLogin2/RegistrationAndLogin/Controllers/GameController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Owin.Logging;
+using RegistrationAndLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class GameController : Controller
+    {
+        public static GameBoard GameBoard = new GameBoard(10);
+        GameBoardController gBController = new GameBoardController(GameBoard);
+
+        // GET: Game
+        public ActionResult Index()
+        {
+            try
+            {
+                if (Request.IsAjaxRequest())
+                {
+                    return PartialView("GameBoard", GameBoard);
+                }
+                else
+                {
+                    return View("Game", GameBoard);
+                }
+            }
+            catch (Exception e)
+            {
+                
+                return RedirectToAction("Index", "Login");
+                
+            }
+            
+        }
+        
+        public PartialViewResult GameBoardView()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Game()
+        {
+            return View(GameBoard);
+        }
+
+        [HttpPost]
+        public ActionResult GameDisplay(GameBoard gameBoard)
+        {
+            //convert gameboard to json string
+
+            //save json string to database
+
+
+            
+            return View(gameBoard);
+        }
+        
+        
+    }
+}
---
+++ RegistrationAndLogin2/RegistrationAndLogin/Models/GameBoard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+
+namespace RegistrationAndLogin.Models
+{
+    [Serializable]
+    //[DataContract]
+    public class GameBoard
+    {
+        //[DataMember]
+        public GameCell[,] GameCells { get; set; }
+        //[DataMember]
+        public int BoardSize { get; set; }
+        
+        public GameBoard(int boardsize)
+        {
+            BoardSize = boardsize;
+            GameCells = new GameCell[boardsize, boardsize];
+        }
+
+        
+    }
+}
